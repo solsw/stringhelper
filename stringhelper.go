3cd3e7885fd3f9ce508448479087c155cac4ee0f
Oleg Subachev <giit@duck.com>
2023-03-10 22:00:22 +0500

    StringToStrings added

--- stringhelper.go
+++ stringhelper.go
@@ -1,10 +1,11 @@
 package stringhelper
 
 import (
+	"bufio"
 	"strings"
 )
 
-// JoinSkip is like strings.Join, but skips the elements for which 'skip' (if any) returns true.
+// JoinSkip is like [strings.Join], but skips the elements for which 'skip' (if any) returns true.
 func JoinSkip(elems []string, sep string, skip func(string) bool) string {
 	var b strings.Builder
 	for _, elem := range elems {
@@ -17,4 +18,15 @@ func JoinSkip(elems []string, sep string, skip func(string) bool) string {
 		b.WriteString(elem)
 	}
 	return b.String()
+}
+
+// StringToStrings slices 's' into all substrings separated by end-of-line markers (see [bufio.ScanLines]).
+// If 's' is empty, empty slice is returned.
+func StringToStrings(s string) []string {
+	ss := []string{}
+	sc := bufio.NewScanner(strings.NewReader(s))
+	for sc.Scan() {
+		ss = append(ss, sc.Text())
+	}
+	return ss
 }
--- stringhelper_test.go
+++ stringhelper_test.go
@@ -1,6 +1,7 @@
 package stringhelper
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -44,6 +45,31 @@ func TestJoinSkip(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := JoinSkip(tt.args.elems, tt.args.sep, tt.args.skip); got != tt.want {
 				t.Errorf("JoinSkip() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStringToStrings(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{name: "01", args: args{s: ""}, want: []string{}},
+		{name: "02", args: args{s: " "}, want: []string{" "}},
+		{name: "1", args: args{s: "2\n3\r\n4"}, want: []string{"2", "3", "4"}},
+		{name: "2", args: args{s: "1\r\n\n3\r\n4"}, want: []string{"1", "", "3", "4"}},
+		{name: "3", args: args{s: "2\n3\r\n4\n"}, want: []string{"2", "3", "4"}},
+		{name: "4", args: args{s: "\r\n2\n3\r\n4\n"}, want: []string{"", "2", "3", "4"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := StringToStrings(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("StringToStrings() = %v, want %v", got, tt.want)
 			}
 		})
 	}
